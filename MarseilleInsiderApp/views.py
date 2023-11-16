9ad768710037f1be2a3643dd5102be11e3a3a549
dervanova <ksenija.dervanova@etu.univ-amu.fr>
2023-11-16 12:06:46 +0100

    continuing templates

--- MarseilleInsiderApp/views.py
+++ MarseilleInsiderApp/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
-
+from MarseilleInsiderApp.models import Loisir,Catégorie
 
 # Create your views here.
 
 def vw_home(request, ):
-    return render(request, "home_page.html")
+    title = "MARSEILLE INSIDER"
+    description = "Explorez les meilleures activités à faire dans la magnifique ville de Marseille"
+    button = "Voir les activités"
+    block_description="Nos Activités Recommandées "
+    return render(request, "home_page.html",
+                  {"title": title, "description": description, "button": button, "block_description":block_description})
+def vw_activities(request, ):
+
+    return render(request, "activitiesloop.html",
+                  {"Loisirs":Loisir.objects.all(),"Categories":Catégorie.objects.all()})
+def loop(request,categorie):
+    loisirs_list=Loisir.objects.filter(categorie=categorie)
+    return render(request,"activitiesloop.html",
+                  {"Loisirs":loisirs_list})
--- projetWEB/urls.py
+++ projetWEB/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from MarseilleInsiderApp.views import vw_home
+from MarseilleInsiderApp.views import vw_home,vw_activities
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/',vw_home),
+    path('activities/',vw_activities),
 ]
