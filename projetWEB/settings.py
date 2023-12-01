70fe30049c65b259c9d7af9fec1e21aba8032634
theo-manon <theto2011@gmail.com>
2023-12-01 12:42:47 +0100

    adding settings bis

--- projetWEB/settings.py
+++ projetWEB/settings.py
@@ -16,11 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATICFILES_DIRS = [
-    '../MarseilleInsiderApp/static/',
-]
 
-STATIC_ROOT = '../MarseilleInsiderApp/'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -127,9 +123,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'MarseilleInsiderApp/static'),
 ]
 
+STATIC_ROOT = '../MarseilleInsiderApp/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
