232f4847667b9da04fbe304306a8f69b88cdf67e
theo-manon <theto2011@gmail.com>
2023-12-01 13:19:09 +0100

    ok

--- projetWEB/settings.py
+++ projetWEB/settings.py
@@ -122,7 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = '/home/manonmantez/manonmantez.pythonanywhere.com/MarseilleInsiderApp/static/'
 
 
 
